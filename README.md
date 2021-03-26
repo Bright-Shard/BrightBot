06f8b9683042dcb9bd8c86bbc04e3325e9d23f65
BrightShard <49419825+Bright-Shard@users.noreply.github.com>
2021-03-26 05:38:29 +0000

    Release 1.0 - Added .meme command to get a meme from teh interwebz - Added a .gmc command to get a Giant Military Cats tweet (https://twitter.com/giantcat9) - Started adding comments to the code

--- README.md
+++ README.md
@@ -10,12 +10,12 @@ Currently, I have the following commands implemented:
 - **RemoveRole** - Removes a role from someone on the server.
 - **Mute** - Adds the 'Mute' role to someone on the server. You will need to manually turn off the permission to send messages in channels where you don't want the muted people to talk.
 - **Unmute** - Removes the 'Mute' role from someone on the server.
-
-I plan to implement more commands in the future, of course. These might include:
-- **ReactionRole** - Reactions to a certain message will give users a certain role.
 - **GMC** - Gets a random tweet from [Giant Military Cats](https://twitter.com/giantcat9). If you don't know what GMC is, it's a twitter account that posts ridiculous military scenes with giant cats in the middle of it all.
  - **Meme** - Gets a random meme from teh interwebz
 
+I plan to implement more commands in the future, of course. These might include:
+- **ReactionRole** - Reactions to a certain message will give users a certain role.
+- **Giveaway** - Starts a giveaway
 
  Some commands will require the `admin_commands` role. These are commands like ban, kick, purge, addrole, removerole, mute, and unmute. This ensures only admins can run administrative commands. **Make sure only people who need those commands have that role.** You currently will need to set that role up yourself, but it's not hard - just make a role with the name `admin_commands`, you don't even need to colour it or give it permissions.
 
@@ -23,6 +23,9 @@ I plan to implement more commands in the future, of course. These might include:
  There are currently 2 options.
  1. **Add the bot to your server** - Just add the bot to your own server. The link is [here]('https://discord.com/api/oauth2/authorize?client_id=745433967486042133&permissions=8&scope=bot').
  2. **Run your own instance of the bot** - Because this bot is open-source, you can run it yourself! I'd recommend doing it on [Replit]('https://replit.com'), unless you have your own cloud service, because Replit is free. The only trouble is, if you are on Replit, you will need to set up some sort of bot to visit the server in `server.py` every so often (Under an hour), since if the webpage doesn't get a visit in 1 hour, Replit will stop the program. The easiest way to do this is via a website uptime monitor, like [MonitorUptime](https://monitoruptime.io) or [UptimeRobot](https://uptimerobot.com), because they are free and will connect to the website every few minutes (To see if it's up, but this will also keep your bot running.)
+
+ # Can't I just use <insert bot name here>?
+ Yeah, you're free to use whatever bot you want - however, if  you run BrightBot, you can see what is going on behind the scenes. With other bots, you don't know what's happening, and, what's more, some of them want money for basic features. That's why I have BrightBot - it's a free alternative, plus, you can run and modify it yourself.
 
  # Licensing
  You can view the full project license in the `LICENSE` file on this repo. However, I figured I'd summarize it here, since I made my own license (Most other licenses were long and complicated or didn't have the requirements I wanted.) It basically says anyone is free to redistribute, modify, or download this project's source code as long as you don't sell it for profit, do keep it open-source, and don't include malware, add trackers, put in ads, or restrict it's use in some way (I don't count language barriers, since translations are pretty difficult). This makes sure any other versions of my software are free, open-source, private, clean/secure, and available to everyone.
--- main.py
+++ main.py
@@ -1,36 +1,59 @@
-import discord, os
-from discord.ext import commands
-from server import keep_alive
-import asyncio
+# BrightBot, made by BrightShard
+# This code is open-source on GitHub: https://github.com/Bright-Shard/BrightBot
 
-botIntents = discord.Intents.default()
+import discord, os # Import discord.py for making the bot and os for getting environment variables
+from discord.ext import commands, tasks # Import the commands sublibrary to make the bot and tasks sublibrary for background processes
+from server import keep_alive # This is the web server, used to keep the bot online so Replit doesn't shut it down
+import asyncio # Used for timed delays
+import random # Used to pick random numbers
+import twitter # Used for getting twitter stuff
+import json # For parsing JSON files like memes
+import requests # For getting remote content
+
+# Twitter API for getting tweets
+twit = twitter.Twitter(auth=twitter.OAuth(
+  os.getenv('ATOKEN'), # Set environment variable ATOKEN to your access token
+  os.getenv('ASECRET'), # Set environment variable ASECRET to your access secret
+  os.getenv('CKEY'), # Set environment variable CKEY to your API key
+  os.getenv('CSEC'))) # Set environment variable CSEC to your API secret
+
+botIntents = discord.Intents.default() # Declare the bot's intents, AKA what permissions it has as a Discord bot
 botIntents.members = True
 
-bot = commands.Bot(command_prefix=".",intents=botIntents)
+bot = commands.Bot(command_prefix=".",intents=botIntents) # Load the bot with the command prefix "."
 
-@bot.event
+global GMCTweets
+GMCTweets = twit.statuses.user_timeline(screen_name='giantcat9',count=30)
+
+@tasks.loop(hours=12.0)
+async def getTwitter():
+  global GMCTweets
+  GMCTweets = twit.statuses.user_timeline(screen_name='giantcat9',count=30)
+
+@bot.event # Print who the bot is logged in as when it starts
 async def on_ready():
     print('Logged in as:')
     print(bot.user.name)
-    print(bot.user.id)
     print('------')
 
-@bot.command(name='addRole',brief='Add a role to a user. Needs admin_commands role.',description='Add a role to someone on the server. You can add multiple roles or just one. You can ping the person and role or just type their names. Needs admin_commands role.',aliases=['addrole','AddRole','Addrole','arole'])
-@commands.has_role('admin_commands')
-async def addRole(ctx, user: discord.Member, *roles: discord.Role):
-  await ctx.send(f'Giving user {user.name} the roles {[i.name for i in roles]}.')
-  for role in roles:
+# The addRole command, used to add a role to a user
+@bot.command(name='addRole',brief='Add a role to a user. Needs admin_commands role.',description='Add a role to someone on the server. You can add multiple roles or just one. You can ping the person and role or just type their names. Needs admin_commands role.',aliases=['addrole','AddRole','Addrole','arole']) # Declare it as a command, set it's brief description, long description, and command aliases
+@commands.has_role('admin_commands') # Make sure whoever is running the command has the admin_commands role
+async def addRole(ctx, user: discord.Member, *roles: discord.Role): # Then make a function to handle the command
+  await ctx.send(f'Giving user {user.name} the roles {[i.name for i in roles]}.') # Sends in the chat who is getting what roles
+  for role in roles: # Iterate through the roles received from the command and add them to the given user
     await user.add_roles(role)
 
-@bot.command(name='removeRole',brief='Remove a role froma user. Needs admin_commands role.',description='Remove a role from someone on the server. You can remove multiple roles or just one. You can ping the person and role or just type their names. Needs admin_commands role.')
-@commands.has_role('admin_commands')
+# The removeRole command, used to remove a user's role
+@bot.command(name='removeRole',brief='Remove a role froma user. Needs admin_commands role.',description='Remove a role from someone on the server. You can remove multiple roles or just one. You can ping the person and role or just type their names. Needs admin_commands role.') # Same as above
+@commands.has_role('admin_commands') # Make sure whoever is running the command has the admin_commands role
 async def removeRole(ctx, user: discord.Member, *roles: discord.Role):
   await ctx.send(f'Removing the roles {[i.name for i in roles]} from user {user.name}.')
   for role in roles:
     await user.remove_roles(role)
 
-@bot.command(name='purge',brief='Cleanse the chat. Of messages. Needs admin_commands role.',description='Deletes a TON of messages.',aliases=['PURGE'])
-@commands.has_role('admin_commands')
+@bot.command(name='purge',brief='Cleanse the chat. Of messages. Needs admin_commands role.',description='Deletes a TON of messages.',aliases=['PURGE']) # Same as above
+@commands.has_role('admin_commands') # Make sure whoever is running the command has the admin_commands role
 async def purge(ctx):
   await ctx.send('Purging the chat in 5...')
   await asyncio.sleep(1)
@@ -46,39 +69,52 @@ async def purge(ctx):
   await asyncio.sleep(1)
   await ctx.channel.purge()
 
-@bot.command(name='mute',brief='Mute someone. Requires the admin_commands role.',description='Adds a Muted role to whoever is mentioned. You can ping them or just type their username.',aliases=['Mute'])
-@commands.has_role('admin_commands')
+@bot.command(name='mute',brief='Mute someone. Requires the admin_commands role.',description='Adds a Muted role to whoever is mentioned. You can ping them or just type their username.',aliases=['Mute']) # Same as above
+@commands.has_role('admin_commands') # Make sure whoever is running the command has the admin_commands role
 async def mute(ctx, user: discord.Member):
   await ctx.send(f'Muting {user.name}.')
   await user.add_roles(discord.utils.get(ctx.guild.roles,name='Muted'))
 
-@bot.command(name='unmute',brief='Unmute someone. Requires the admin_commands role.',description='Removes the Muted role from whoever is mentioned. You can ping them or just type their username.',aliases=['Unmute','unMute','UnMute'])
-@commands.has_role('admin_commands')
+@bot.command(name='unmute',brief='Unmute someone. Requires the admin_commands role.',description='Removes the Muted role from whoever is mentioned. You can ping them or just type their username.',aliases=['Unmute','unMute','UnMute']) # Same as above
+@commands.has_role('admin_commands') # Make sure whoever is running the command has the admin_commands role
 async def unmute(ctx, user: discord.Member):
   await ctx.send(f'Unmuting {user.name}')
   await user.remove_roles(discord.utils.get(ctx.guild.roles,name='Muted'))
 
-@bot.command(name='ban',brief='Ban someone from the server. Needs the admin_commands role.',description='Bans whoever is mentioned. You can ping them or just type their name.',aliases=['Ban','banhammer','BanHammer','banHammer','Banhammer','hammer','Hammer','HAMMER'])
-@commands.has_role('admin_commands')
+@bot.command(name='ban',brief='Ban someone from the server. Needs the admin_commands role.',description='Bans whoever is mentioned. You can ping them or just type their name.',aliases=['Ban','banhammer','BanHammer','banHammer','Banhammer','hammer','Hammer','HAMMER']) # Same as above
+@commands.has_role('admin_commands') # Make sure whoever is running the command has the admin_commands role
 async def ban(ctx, user: discord.Member, *banReason: str):
-  finalReason = ''
-  for i in banReason:
-    finalReason += i
-  finalReason += '\n Banned by '
-  finalReason += {ctx.author}
-  await user.ban(reason=finalReason)
   await ctx.send(f'{user.name} was HAMMERED by {ctx.author}, RIP.')
+  finalReason = str(banReason)
+  finalReason += '(Banned by ' + str({ctx.author}) + ')'
+  await user.ban(reason=finalReason)
 
-@bot.command(name='kick',brief='Kick someone from the server. Needs the admin_commands role.',description='Kicks the person named in this command. You can ping them or just type out their username.',aliases=['Kick','remove','Remove'])
-@commands.has_role('admin_commands')
+@bot.command(name='kick',brief='Kick someone from the server. Needs the admin_commands role.',description='Kicks the person named in this command. You can ping them or just type out their username.',aliases=['Kick','remove','Remove']) # Same as above
+@commands.has_role('admin_commands') # Make sure whoever is running the command has the admin_commands role
 async def kick(ctx, user: discord.Member, *kickReason: str):
-  finalReason = ''
-  for i in kickReason:
-    finalReason += i
-  finalReason += '\n Kicked by '
-  finalReason += {ctx.author}
-  await user.kick(reason=finalReason)
   await ctx.send(f'{ctx.author} decided {user.name} could no longer be with us.')
+  finalReason = str(kickReason) + '(Kicked by ' + str({ctx.author}) + ')'
+  await user.kick(reason=finalReason)
+
+@bot.command(name='meme',brief='Gets a random meme from teh interwebz.',description='Gets a meme from https://meme-api.herokuapp.com/gimme, then sends it in the chat.',aliases=['Meme'])
+async def meme(ctx):
+  content = requests.get('https://meme-api.herokuapp.com/gimme')
+  refinedContent = json.loads(content.text)
+  msg = discord.Embed(title=refinedContent['title'])
+  msg.set_image(url=refinedContent['url'])
+  await ctx.send(embed=msg)
+
+@bot.command(name='GMC',brief='Gets a random tweet from Giant Military Cats.',description='Uses nitter.net and RSS to get a random tweet from Giant Military Cats (https://twitter.com/giantcat9).',aliases=['gmc','cat','giantcat'])
+async def GMC(ctx):
+  try:
+    tweet = GMCTweets[random.randint(0,29)]
+    text = tweet['text'] + '\n Source: https://twitter.com/giantcat9'
+    img = tweet['entities']['media'][0]['media_url_https']
+    msg = discord.Embed(title='Giant Military Cats',link='https://twitter.com/giantcat9',description=text)
+    msg.set_image(url=img)
+    await ctx.send(embed=msg)
+  except:
+    await GMC(ctx=ctx)
 
 keep_alive()
 bot.run(os.getenv('TOKEN'))
